b69afa89d199c22f5dde074f73e42a7a905a1317
TerraNova4697 <nikitabechthold@gmail.com>
2024-07-30 10:38:08 +0500

    Modified daemon installation script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install -y python3 python3-pip python3-dev
 sudo pip3 install -r requirements.txt
-sudo mv cuba-school-bell.service /lib/systemd/system/cuba-school-bell.service
+touch log/debug.log && touch log/development.log && touch log/production.log && touch cronjobs.log
 mv env_example .env
+sudo mv cuba-school-bell.service /lib/systemd/system/cuba-school-bell.service
 sudo systemctl daemon-reload
