f440c07ddff0678f368a5013b917705767931783
Nikita <nikitabechthold@gmail.com>
2024-08-06 10:21:53 +0000

    fixed setup file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 sudo apt update
-sudo apt install -y python3 python3-pip python3-dev vlc alsa-utils
+sudo apt install -y python3 python3-pip python3-dev vlc alsa-utils pulseaudio
 sudo pip3 install -r requirements.txt
-mkdir log
-touch log/debug.log && touch log/development.log && touch log/production.log && touch cronjobs.log
-mv env_example .env
+sudo mkdir log
+sudo touch log/debug.log && sudo touch log/development.log && sudo touch log/production.log && sudo touch cronjobs.log
+sudo mv env_example .env
 sudo mv cuba-school-bell.service /lib/systemd/system/cuba-school-bell.service
 sudo chown -R $USER:$USER .
 sudo usermod -aG audio,pulse,pulse-access $USER
