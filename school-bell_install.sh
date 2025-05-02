476b5ff4cf3547cbde8438ca80a17e43d2aa266f
Nik <nikitabechthold@gmail.com>
2025-05-02 11:16:32 +0500

    Improved bash scripts

--- school-bell_install.sh
+++ school-bell_install.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-mv env_example .env
+echo "Installing School Bell app..."
+cp env_example .env
 
 sudo mkdir -p /usr/share/school_bell && sudo chown -R cuba:cuba /usr/share/school_bell
 sudo mkdir -p /var/log/school_bell && sudo chown -R cuba:cuba /var/log/school_bell
 
+echo "Downloading apt dependencies..."
 sudo apt update && sudo apt install python3-pip python3.12-venv vlc \
     libvlc-dev python3-vlc alsa-utils pulseaudio sox libsox-fmt-all -y
 
+# Set default audiofiles
 cp defaultAudio.mp3 /usr/share/school_bell/startLessonAudio.mp3
 cp defaultAudio.mp3 /usr/share/school_bell/endLessonAudio.mp3
 cp defaultAudio.mp3 /usr/share/school_bell/alarmAudio.mp3
@@ -17,10 +20,13 @@ cp defaultAudio.mp3 /usr/share/school_bell/fireAudio.mp3
 cp defaultAudio.mp3 /usr/share/school_bell/ambulanceAudio.mp3
 cp defaultAudio.mp3 /usr/share/school_bell/testAudio.mp3
 
+echo "Downloading python dependencies..."
 python3 -m venv .venv && \
     .venv/bin/pip install -r requirements.txt
 
-
+echo "Setting daemons..."
 sudo cp cuba-school-bell.service /etc/systemd/system
 sudo cp ps_killer/ps-killer.service /etc/systemd/system
 sudo systemctl daemon-reload
+
+echo "School Bell installed.\nDon't forget to set your ENV in .env file.\n"
--- school-bell_uninstall.sh
+++ school-bell_uninstall.sh
@@ -5,17 +5,26 @@ LOG_DIR=/var/log/school_bell/
 
 echo "Deleting school-bell app components..."
 sudo rm -rf /usr/share/school_bell
+echo "School Bell app components deleted."
 
-for arg in "$@"; do
-    if [ "$arg" == "--logs" ]; then
-        echo "Deleting logs from $LOG_DIR..."
+read -p "Do you wish to delete logs: $LOG_DIR? (y/n): " ANSWER
+case "$answer" in
+    y|Y)
         if [ -d "$LOG_DIR" ]; then
-            sudo rm -rf $LOG_DIR
+            echo "Deleting logs..."
+            sudo rm -rf "$LOG_DIR"
             echo "Logs deleted."
+        else
+            echo "ℹ️ Папка логов не найдена: $LOG_DIR"
         fi
-        break
-    fi
-done
+        ;;
+    n|N)
+        echo "Skipping logs."
+        ;;
+    *)
+        echo "Unknown answer. Skipping logs."
+        ;;
+esac
 
 echo "Stopping app services..."
 sudo systemctl stop cuba-school-bell.service
@@ -23,10 +32,13 @@ sudo systemctl disable cuba-school-bell.service
 
 sudo systemctl stop ps-killer.service
 sudo systemctl disable ps-killer.service
+echo "App services stopped."
 
 echo "Deleting app services..."
 sudo rm /etc/systemd/system/cuba-school-bell.service
 sudo rm /etc/systemd/system/ps-killer.service
 sudo systemctl daemon-reload
+echo "App services deleted."
+
 
 echo "School Bell App deleted."
